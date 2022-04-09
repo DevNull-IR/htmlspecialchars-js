ff7d81421a78a76c509438a17637548b055d2fe1
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-04-09 15:19:41 +0430

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const htmlspecialchars = (string) => {
+            // &gt;
+            let htmlstr = string;
+            for(let i=0; i < htmlstr.length; i++){
+                htmlstr = htmlstr.replace('>','&gt;');
+                htmlstr = htmlstr.replace('<','&lt;');
+            }
+            
+            
+            return htmlstr;
+        };
