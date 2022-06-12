d1967757c7040382fcdea108cf57d4e30307caf5
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-06-12 12:06:33 +0430

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const htmlspecialchars = (string) => {
     for(let i=0; i < htmlstr.length; i++){
         htmlstr = htmlstr.replace('>','&gt;');
         htmlstr = htmlstr.replace('<','&lt;');
+        htmlstr = htmlstr.replace('"','&quot;');
+        htmlstr = htmlstr.replace('\'','&#039;');
     }
     return htmlstr;
 };
